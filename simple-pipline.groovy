67520214bfc8b0550426106b409338f30b81bb6a
fuckyou <none>
2022-11-17 01:35:29 +0300

    fix

--- simple-pipline.groovy
+++ simple-pipline.groovy
@@ -1,10 +1,21 @@
+
+
+
+vi_module "some-module.groovy"
+
 pipeline {
     agent any
     stages {
         stage('Stage 1') {
             steps {
                 echo 'Hello world!'
+                echo "${a}"
+                throw 1
             }
         }
     }
+}
+
+def vi_module(String path) {
+    evaluate(new File(path))
 }
