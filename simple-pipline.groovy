23a1315e75ccc051b70344a3554960d0c5b1e542
fuckyou <none>
2022-11-17 01:51:59 +0300

    fix

--- simple-pipline.groovy
+++ simple-pipline.groovy
@@ -1,13 +1,14 @@
 
-def some_pipeline = load 'some-pipeline.groovy'
+def some_pipeline_module
 
 pipeline {
     agent any
     stages {
         stage('Stage 1') {
+            some_pipeline_module = load 'some-pipeline.groovy'
             steps {
                 echo 'Hello world!'
-                echo "${some_pipeline.a}"
+                echo "${some_pipeline_module.a}"
             }
         }
     }
