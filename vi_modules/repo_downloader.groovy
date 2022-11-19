af362b9534229c4e7ed41df65fd29f71bd3ca43d
fuckyou <none>
2022-11-19 04:36:09 +0300

    fix

--- vi_modules/repo_downloader.groovy
+++ vi_modules/repo_downloader.groovy
@@ -5,6 +5,9 @@ getRepositories = { config ->
             checkout([
                 $class: 'GitSCM',
                 branches: [[name: "*/${repo.branch}/*"]],
+                extensions: [
+                        [$class: 'CloneOption', noTags: true, reference: '', shallow: false]
+                ],
                 userRemoteConfigs: [[url: "${repo.url}"]]
             ])
             sh 'touch file'
@@ -16,10 +19,12 @@ getRepositories = { config ->
 pushRepositories = { config ->
     config.repos.each { repoName, repo ->
         dir("${config.from}/${repoName}") {
-            sh "git checkout origin/${repo.branch}"
-            sh 'git add .'
-            sh 'git commit -m "From Jenkins"'
-            sh "git push"
+            withCredentials([gitUsernamePassword(credentialsId: "${repo.credsId}", gitToolName: 'Default')]) {
+                sh "git checkout ${repo.branch}"
+                sh 'git add .'
+                sh 'git commit -m "From Jenkins"'
+                sh "git push"
+            }
         }
     }
 }
